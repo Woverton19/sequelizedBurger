c9d32db744223c348ab57c60b6c4517f4d3d56c1
John Overton <John Overton>
2016-11-17 21:34:58 -0600

    first push

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name) VALUES ('The Big Mick');
+
+INSERT INTO burgers (burger_name) VALUES ('WhataChicken');
+
+INSERT INTO burgers (burger_name) VALUES ('Sour Dough Jack');
+
+INSERT INTO burgers (burger_name) VALUES ('Bell Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheese Burger');
