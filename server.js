b13e869d5d215d4a4cc85f7603ab46357d67998c
John Overton <John Overton>
2016-11-19 14:19:35 -0600

    code is not working the way I want having trouble with sequilized

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var models = require("./models");
 var methodOverride = require('method-override');
-
 var app = express();
+
 
 app.use(express.static(process.cwd() + '/public'));
 
@@ -12,13 +13,17 @@ app.use(bodyParser.urlencoded({
 
 app.use(methodOverride('_method'));
 var exphbs = require('express-handlebars');
+
 app.engine('handlebars', exphbs({
 	defaultLayout: 'main'
 }));
+
 app.set('view engine', 'handlebars');
 
 var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
-var port = 3000;
-app.listen(port);
+var Port = process.env.Port ||3000;
+app.listen(Port, function(){
+	console.log("listening on port....." + Port);
+});
